6bee6ce0e9dd0b45ea314c394a2c15ab42594581
techatlasdev <srgoqui@gmail.com>
2025-06-16 11:43:12 -0500

    Mejorando bugs

--- mario_bross/entidades/entidad.py
+++ mario_bross/entidades/entidad.py
@@ -2,20 +2,14 @@ import pygame
 
 class Entidad:
   def __init__(self, x, y, width, height, velocidad=5, color=(255, 255, 255)) -> None:
-    # Atributos de posición y tamaño
     self.rect = pygame.Rect(x, y, width, height)
-
-    # Atributos de movimiento
     self.velocidad = velocidad
     self.vel_x = 0
     self.vel_y = 0
     self.en_suelo = False
 
-    # Atributos de física
     self.fuerza_salto = -15
     self.gravedad = 0.8
-
-    # Atributos visuales
     self.color = color
 
   def manejar_eventos(self, events, keys_pressed):
@@ -28,9 +22,8 @@ class Entidad:
     """
     Actualiza el estado de la entidad. Lógica de IA o física básica va aquí.
     """
-    # Aplicamos la gravedad por defecto
     self.vel_y += self.gravedad
-    if self.vel_y > 10: # Velocidad terminal para no acelerar infinitamente
+    if self.vel_y > 10:
         self.vel_y = 10
   
   def _render_(self, screen):
@@ -39,7 +32,7 @@ class Entidad:
     """
     raise NotImplementedError("Debe implementar el metodo _render_")
 
-  def draw(self, screen):
+  def dibujar(self, screen):
     self._render_(screen)
 
   def mover_derecha(self):
